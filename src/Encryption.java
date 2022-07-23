df1d5c79e2c45feff9d71cae30bf6ef0b6c9afbb
Bogdan <bandrashko1@gmail.com>
2022-07-23 22:23:10 +0200

    Building an encryption program on Java

---
+++ src/Encryption.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Encryption extends JFrame {
+
+    Encryption(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+
+}
